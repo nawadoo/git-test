7ca9f47ac1c5257369fa4021a0474bfc62517273
ciree <nawadoo@gmail.com>
2012-11-30 21:31:16 +0100

    comment 2

--- gitTest/src/com/example/gittest/MainActivity.java
+++ gitTest/src/com/example/gittest/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     }
     
     //hello comment
-
+    // comment 2
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
